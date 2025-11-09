a8d90ea95f89f199e3274922d3b9f3a5a62a8998
Ian Lam <ianklam09@gmail.com>
2025-11-09 16:28:12 -0500

    removed additional image folder, test macenko

--- preprocessing.py
+++ preprocessing.py
@@ -4,10 +4,8 @@ from sklearn.decomposition import PCA
 import cv2
 from pathlib import Path
 
-
-
 def load_images():
-    image_dir = Path(__file__).parent / "images" / "images"
+    image_dir = Path(__file__).parent / "images"
     file = Path(__file__).parent / "image_files_info.txt"
 
     images = []
@@ -30,8 +28,12 @@ def macenko_rgb(image_path):
     pca.fit(reshape_od)
     stain_m = pca.components_.T
     stains = np.dot(reshape_od, np.linalg.pinv(stain_m).T)
+
+    ##hardcoded target means and stds. pick a target.
     target_means = np.array([0.5, 0.5])
     target_stds = np.array([0.2, 0.2])
+    ##
+
     stains_normalized = (stains - np.mean(stains, axis=(0, 1))) / np.std(stains, axis=(0, 1))
     stains_normalized = stains_normalized * target_stds + target_means
     od_normalized = np.dot(stains_normalized.reshape((-1, 2)), stain_m.T)
@@ -39,6 +41,7 @@ def macenko_rgb(image_path):
     img_reconstructed = np.exp(-od_normalized)
     img_reconstructed = np.clip(img_reconstructed, 0, 1)
     img_reconstructed = (img_reconstructed * 255).astype(np.uint8)
+    return img_reconstructed
 
 
 def min_max_rgb(image_path):
@@ -50,4 +53,4 @@ def min_max_rgb(image_path):
 
 if __name__ == "__main__":
     load_images()
-
+    #cv2.imwrite("test aaf.png", macenko_rgb("images/images/MHIST_aaf.png"))
